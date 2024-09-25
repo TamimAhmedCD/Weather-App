c9111c0695d25938419327625c655d3f91bf1c94
Tamim Ahmed <tamim20072@gmail.com>
2024-09-25 14:09:58 +0600

    all features added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon')
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -14,6 +15,24 @@ async function checkWeather(city) {
     document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c';
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
     document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h';
+
+    if (data.weather[0].main == 'Clouds') {
+        weatherIcon.src = './Assets/clouds.png'
+    }
+    else if (data.weather[0].main == 'Clear') {
+        weatherIcon.src = './Assets/clear.png'
+    }
+    else if (data.weather[0].main == 'Rain') {
+        weatherIcon.src = './Assets/Rain.png'
+    }
+    else if (data.weather[0].main == 'Drizzle') {
+        weatherIcon.src = './Assets/drizzle.png'
+    }
+    else if (data.weather[0].main == 'Mist') {
+        weatherIcon.src = './Assets/mist.png'
+    }
+    
+    document.querySelector('.weather').style.display = 'block';
 }
 
 searchBtn.addEventListener('click', ()=> {
